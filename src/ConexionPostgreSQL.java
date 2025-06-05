0178f56dfb82ec6ecbd8c4af0f07d9455774cfd6
Borja De Saa <bdesaavazquez@danielcastelao.org>
2025-06-05 10:31:41 +0200

    cambio contraseña BD

--- src/ConexionPostgreSQL.java
+++ src/ConexionPostgreSQL.java
@@ -7,7 +7,7 @@ public class ConexionPostgreSQL{
     public static void main(String[] args) {
         String url = "jdbc:postgresql://aws-0-us-east-2.pooler.supabase.com:6543/postgres";
         String usuario = "postgres.lhadhhdyxuiytabqmnnt";
-        String contrasena = "ZV2SsqHEBCAhdk8Z";
+        String contrasena = "transportesSAcontraseña";
 
         try (Connection conexion = DriverManager.getConnection(url, usuario, contrasena)) {
             System.out.println("Conexión establecida correctamente.");
